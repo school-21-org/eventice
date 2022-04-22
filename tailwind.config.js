98b5f2a9faca1a36b6619dad22a543bde2f5422a
mdaniell <mdaniell@student.21-school.ru>
2022-04-23 01:24:33 +0700

    feat: added bellota and comfortaa font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bellota: "'Bellota Text', cursive",
+        comfortaa: "'Comfortaa', cursive",
+      }
+    },
   },
   plugins: [],
 }
