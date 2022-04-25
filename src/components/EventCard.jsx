f03e32ab9fdf7abcbce2a30bf9c0f377634bd818
Aleksey Skhomenko <a@skhom.ru>
2022-04-26 01:24:38 +0700

    overwrite

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-export const EventCard = () => (
-  <div className="font-bellota select-none border border-black rounded-xl xl:w-3/5 py-2 px-4">
-    <h2 className="text-center border-b border-black font-bold text-3xl">Hello World!</h2>
-    <p className="mb-0 text-right text-2xl">4:20</p>
-    <p className="mb-0 text-right text-2xl">10.11.12</p>
-    <p className="mb-0 text-justify pt-12 px-8">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod,
-      nisi vel facilisis mollis, elit tellus congue libero, elementum sodales lectus orci in
-      ipsum. Donec id eleifend libero. Integer euismod eu orci quis lacinia. Suspendisse in
-      faucibus tellus, at imperdiet mauris. Sed vel augue finibus, iaculis metus consectetur,
-      gravida odio. Aliquam vestibulum lobortis sem at maximus. Donec laoreet ornare urna sit
-      amet ornare. Vivamus porta ac eros at sodales. Donec turpis ex, vulputate ut odio eget,
-      rhoncus lacinia odio. Sed bibendum quis magna sed aliquam.
-    </p>
-  </div>
-);
+export const EventCard = ({ name, time, date, description }) => {
+  console.log('name, time, date, description', name, time, date, description);
+  return (<div className="font-bellota select-none border border-black rounded-xl xl:w-2/5 py-2 px-4">
+      { !name && <p className="mb-0 pt-5 text-center text-2xl">Выберите событие на календаре</p> }
+      { name && <>
+          <h2 className="text-center border-b border-black font-bold text-3xl">{name}</h2>
+          <p className="mb-0 text-right text-2xl">{time}</p>
+          <p className="mb-0 text-right text-2xl">{date}</p>
+          <p className="mb-0 text-justify pt-12 px-8">{description}</p>
+        </>
+      }
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar } from 'antd';
 import { EventCard } from '../components/EventCard';
 
-export const Home = () => (
-  <>
-    <Calendar className="xl:w-2/5 font-bellota" />
-    <EventCard />
+const serverData = [
+  {
+    date: '2022-04-11',
+    time: '4:20',
+    name: 'Mafia',
+    description: 'LORA IPSA',
+  },
+  {
+    date: '2022-05-14',
+    time: '4:20',
+    name: 'Mafia',
+    description: 'LORA IPSAAAAAAAAAAAAAAAA',
+  },
+];
+
+const showEventCard = (value, setEvent) => {
+  console.log('value, setEvent', value, setEvent);
+  const dataFromServer = serverData;
+  const DateValue = value.format('yyyy-MM-DD');
+  const listData = dataFromServer.filter((item) => item.date === DateValue);
+  setEvent(listData);
+}
+
+const getListData = (value) => {
+  const dataFromServer = serverData;
+  const DateValue = value.format('yyyy-MM-DD');
+  return dataFromServer.filter((item) => item.date === DateValue);
+}
+
+const getEventData = (value) => {
+  const listData = getListData(value);
+
+  return (
+    <ul>
+      {listData.map((item) => (
+        <li key={item.date}>
+          <p className="mb-0">{item.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export const Home = () => {
+  const [selectedEvents, setSelectedEvents] = useState([]);
+
+  return <>
+    <Calendar onSelect={(e) => showEventCard(e, setSelectedEvents)} dateCellRender={getEventData} className="xl:w-3/5 font-bellota" />
+    <EventCard name={selectedEvents[0]?.name} date={selectedEvents[0]?.date} time={selectedEvents[0]?.time} description={selectedEvents[0]?.description} />
   </>
-);
+};
